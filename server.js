20a484245e23711237c9f1735443a9605964907c
BBBIJI <liekenny273@gmail.com>
2024-03-21 19:11:10 +0800

    fixed bugs for deplying, and added breakfast, veg and, main buffet pages

--- server.js
+++ server.js
@@ -1,34 +1,27 @@
-
+//Dependencies
 const express = require('express')
 const app = express()
 const port = 3000
 app.set("view engine","ejs")
 
-
+//GET function for the main store page
 app.get('/',(req,res)=>{
   res.render("main.ejs")
 })
 
-const notes= [
-  {
-    title: "my first note",
-    contents : "hello world"
-},
-{
-  title: "my second note",
-    contents : "hello world2"
-}
-]
 
-app.get('/notes',(req,res)=>{
-  res.render("notes.ejs",{
-    notes
-  })
-})
-
+//GET functions for all the stores
 app.get('/VEG',(req,res)=>{
   res.render("VEG.ejs")
 })
+app.get('/mainbuffet',(req,res)=>{
+  res.render("mainbuffet.ejs")
+})
+app.get('/breakfast',(req,res)=>{
+  res.render("breakfast.ejs")
+})
+
+
 app.use(express.static("public"))
 //how to call
 //localhost:3000/index.html
